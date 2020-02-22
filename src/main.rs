ef5844db2bd8b355e3202874a87df2ea0587fa3b
mikemehl <michaelalanmehl@gmail.com>
2020-02-22 17:46:16 -0500

    Much better skeleton, I think easier to understand?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 pub mod CrabChatServer
 {
     use std::sync::{Arc, Mutex, mpsc};
-    use std::io::{Read, Write};
+    use std::io::{Read, Write, ErrorKind};
     use std::net::{TcpListener, TcpStream};
     use std::thread;
 
     enum ThreadMsg
     {
-        NewConnection(Arc<Mutex<TcpStream>>),
+        NewConnection(mpsc::Sender<ThreadMsg>),
         NewMessage(String),
     }
 
@@ -22,7 +22,7 @@ pub mod CrabChatServer
             write_thread(rx);
         });
 
-        for mut stream in listener.incoming() 
+        for stream in listener.incoming() 
         {
             match stream
             {
@@ -43,24 +43,73 @@ pub mod CrabChatServer
         Ok(())
     }
 
-    fn add_client(stream : TcpStream, 
-                  tx : mpsc::Sender<ThreadMsg>)
+    fn add_client(mut stream : TcpStream, 
+                  tx_write : mpsc::Sender<ThreadMsg>)
     {
         println!("Added new client...");
         thread::spawn(move ||
         {
             // The read thread for this client.
+            // Setup a communication channel with the write thread.
+            let (tx, rx) : (mpsc::Sender<ThreadMsg>, mpsc::Receiver<ThreadMsg>) = mpsc::channel();
+            let new_connection_msg = ThreadMsg::NewConnection(tx);
+            tx_write.send(new_connection_msg);
+
+            // Now, we start looping.
             loop
             {
+                // First, check if we've received anything to echo from the write thread.
+                let write_thread_msg = rx.try_recv();
+                match(write_thread_msg)
+                {
+                    Ok(msg) =>
+                    {
+                        // TODO: Send the message to the client! 
+                    },
+                    Err(e) => { println!("ERROR RECEIVING MESSAGE FROM WRITE THREAD"); }
+                }
+
+                // Then, check for any incoming messages from the socket.
+                let mut data_buf = [0u8; 1024];
+                let socket_msg = stream.read_to_end(&mut data_buf.to_vec());
+                match(socket_msg)
+                {
+                    Ok(msg) =>
+                    {
+                        // TODO: Send the message to the write thread!   
+                    },
+                    Err(ref e) if e.kind() == ErrorKind::WouldBlock =>
+                    {
+                        // TODO: How to handle this case? 
+                    }
+                    Err(e) => { println!("SOMETHING EVIL"); }
+                }
             }
         });
     }
 
     fn write_thread(rx : mpsc::Receiver<ThreadMsg>)
     {
+        let mut channel_list : Vec<mpsc::Sender<ThreadMsg>> = Vec::new();
         loop
         {
             let next_msg = rx.recv();
+            match(next_msg)
+            {
+                Ok(msg) =>
+                {
+                    // Handle the message by type.
+                    match(msg)
+                    {
+                        ThreadMsg::NewConnection(tx_channel) => { channel_list.push(tx_channel); },
+                        ThreadMsg::NewMessage(msg) =>
+                        {
+                            // TODO: Send the message to all threads.
+                        }
+                    }
+                },
+                Err(e) => { println!("Error receiving message!!!"); }
+            }
         }
     }
 }
